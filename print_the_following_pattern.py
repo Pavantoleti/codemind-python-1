433901bcee9366de557a7ce81ee327cc8235c1d1
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-21 06:36:41 +0530

    a= int(input()) st=['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] for i in range(a):     for j in range(a):         print(st[i],"",end="")     print(""),

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
-a=int(input())
-for i in range (a):
-    for j in range (a,0,-1):
-        print(j,end=" ")
-    print("")
+a= int(input())
+st=['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(a):
+    for j in range(a):
+        print(st[i],"",end="")
+    print(""),
