a1bddb8607d2c83769463296d495126019a92636
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-24 09:40:38 +0530

    a=int(input()) lst=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] for i in range(a,0,-1):     for j in range(i):         print(lst[i-1],end=" ")     print(""),

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,6 @@
 a=int(input())
-for i in range(a):
-    for j in range(a-(i+1)):
-        print(" ",end="");
-    for j in range(i,0,-1):
-        print(j,end="")
-    for j in range(i+1):
-        print(j,end="")
+lst=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(a,0,-1):
+    for j in range(i):
+        print(lst[i-1],end=" ")
     print(""),
