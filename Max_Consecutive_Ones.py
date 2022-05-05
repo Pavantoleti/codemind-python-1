7024c1ae62dd98fc44e2cadb3b306ae38af9a084
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-05-05 06:55:11 +0530

    a=int(input())
    arr=list(map(int,input().split()))
    m=0
    for i in range(a):
        c=0
        for j in range(i,a):
            if(arr[j]==1):
                c+=1
            else:
                break
        if(m<c):
            m=c
    print(m)

---
+++ Max_Consecutive_Ones.py
@@ -0,0 +1,14 @@
+a=int(input())
+
+arr=list(map(int,input().split()))
+m=0
+for i in range(a):
+    c=0
+    for j in range(i,a):
+        if(arr[j]==1):
+            c+=1
+        else:
+            break
+    if(m<c):
+        m=c
+print(m)
